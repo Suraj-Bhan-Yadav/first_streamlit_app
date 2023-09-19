00cbfbca20970dc595a1abc69365366b2bf6cb4e
Surajbhan Yadav <122910244+Suraj-Bhan-Yadav@users.noreply.github.com>
2023-09-19 11:07:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 st.title('My First Streamlit App')
-st.header('Breakfast Menu')
-st.text('Omega 3 and BlueBerry Oatmeal')
-st.text('Kale spintch and Rocket Smoothie')
-st.text('hard Boiled egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
