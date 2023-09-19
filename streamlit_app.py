f9629b28ac4260d9568b731ba0032c88c3bce33f
Surajbhan Yadav <122910244+Suraj-Bhan-Yadav@users.noreply.github.com>
2023-09-19 11:39:22 +0530

    Update streamlit_app.py
    Third commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 st.title('My First Streamlit App')
 st.header('Breakfast Menu')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -7,4 +8,7 @@ st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avacodo Toast')
  
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")c
+st.dataframe(my_fruit_list)
 
