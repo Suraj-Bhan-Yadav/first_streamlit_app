7c0ec8537787908fd45a9a8010ccb160d8ea2bc4
Surajbhan Yadav <122910244+Suraj-Bhan-Yadav@users.noreply.github.com>
2023-09-21 17:33:09 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,31 @@
 import streamlit as st
-st.title('My First Streamlit App')
-st.header('Breakfast Menu')
-st.text('🥣 Omega 3 & Blueberry Oatmeal')
-st.text('🥗 Kale, Spinach & Rocket Smoothie')
-st.text('🐔 Hard-Boiled Free-Range Egg')
-st.text('🥑🍞 Avacodo Toast')
- 
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected=st.multiselect("Pick some fruits:", list(my_fruit_list.index),['Grapefruit','Grapes'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-st.dataframe(fruits_to_show)
+import pandas as pd
+import json
+import snowflake.snowpark as snowpark
+from snowflake.snowpark import Session, DataFrame
+import utils as u
 
+# connect to Snowflake
+with open('../creds.json') as f:
+    connection_parameters = json.load(f)  
+session = Session.builder.configs(connection_parameters).create()
+
+
+def loadInferAndPersist(file) -> snowpark.DataFrame:
+    file_df = pd.read_csv(file)
+    snowparkDf=session.write_pandas(file_df,file.name,auto_create_table = True, overwrite=True)
+    return snowparkDf
+
+st.header("Grandma data uploader")
+file = st.file_uploader("Drop your CSV here", type={"csv"})
+if file is not None:
+    df= loadInferAndPersist(file)
+    st.subheader("Great, your data has been uploaded to Snowflake!")
+    
+    with st.expander("Technical information"):
+        
+        u.describeSnowparkDF(df)
+        st.write("Data loaded to Snowflake:")
+        st.dataframe(df)
+    
+ 
